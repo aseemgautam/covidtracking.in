499deef1b1dc2a5a9f3b1bc6b3e0a1646ceaf3cc
Aseem Gautam <aseem.gautam@gmail.com>
2020-05-07 06:35:38 +0530

    updated og tag

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ function Index() {
 				<meta property="og:title" content="Understandable insights on covid-19 in India" key="title" />
 				<meta property="og:description" content="Data driven insights & reports on spread of covid-19 in India." key="description" />
 				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://cvd19.in" />
+				<meta property="og:url" content="https://covid19wiki.now.sh/" />
 				<meta property="og:image" content="/cvd-og.png" />
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 			</Head>
