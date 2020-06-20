25cef11816016d941d3fadafb5d7e9b679a70125
Aseem Gautam <aseem.gautam@gmail.com>
2020-06-20 21:59:20 +0530

    commented analytics

--- pages/analytics.js
+++ pages/analytics.js
@@ -1,11 +1,11 @@
 import FullWidthRow from '../components/layout/FullWidthRow';
-import NewCasesChart from '../components/charts/NewCasesChart';
-import TestingChart from '../components/charts/Testing';
-import ActiveCasesSection from '../components/ActiveCasesSection';
-import Analytics from '../classes/Analytics';
-import CovidDataIndia from '../classes/CovidDataIndia';
+// import NewCasesChart from '../components/charts/NewCasesChart';
+// import TestingChart from '../components/charts/Testing';
+// import ActiveCasesSection from '../components/ActiveCasesSection';
+// import Analytics from '../classes/Analytics';
+// import CovidDataIndia from '../classes/CovidDataIndia';
 
-const analytics = ({ testingData }) => {
+const analytics = () => {
 	return (
 		<>
 			<FullWidthRow>
@@ -14,35 +14,35 @@ const analytics = ({ testingData }) => {
 					present the data in the clearest and most accurate way possible.
 				</h2>
 				<h3 className="title">NEW / ACTIVE CASES, DEATHS & RECOVERED</h3>
-				<NewCasesChart cases={Analytics.cases} />
+				{/* <NewCasesChart cases={Analytics.cases} /> */}
 			</FullWidthRow>
-			<FullWidthRow>
+			{/* <FullWidthRow>
 				<h3 className="title">COVID-19 TESTING</h3>
 				<TestingChart testingData={testingData} />
 			</FullWidthRow>
 			<FullWidthRow>
 				<ActiveCasesSection />
-			</FullWidthRow>
+			</FullWidthRow> */}
 		</>
 	);
 };
 
-export async function getStaticProps() {
-	const data = await CovidDataIndia.fetchTests();
-	const testingData = [];
-	data.forEach(test => {
-		testingData.push(
-			{ date: test.date, type: 'Samples', value: test.newSamples },
-			{ date: test.date,
-				type: 'Positive',
-				value: test.newPositive,
-				percent: Math.round((test.percentPositive + Number.EPSILON) * 100) / 100
-			}
-		);
-	});
-	return {
-		props: { testingData }, // will be passed to the page component as props
-	};
-}
+// export async function getStaticProps() {
+// 	const data = await CovidDataIndia.fetchTests();
+// 	const testingData = [];
+// 	data.forEach(test => {
+// 		testingData.push(
+// 			{ date: test.date, type: 'Samples', value: test.newSamples },
+// 			{ date: test.date,
+// 				type: 'Positive',
+// 				value: test.newPositive,
+// 				percent: Math.round((test.percentPositive + Number.EPSILON) * 100) / 100
+// 			}
+// 		);
+// 	});
+// 	return {
+// 		props: { testingData }, // will be passed to the page component as props
+// 	};
+// }
 
 export default analytics;
