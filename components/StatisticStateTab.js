60048128a194665b666b14aa4ed073234a861db2
Aseem Gautam <aseem.gautam@gmail.com>
2020-07-07 20:48:35 +0530

    live update

--- components/StatisticStateTab.js
+++ components/StatisticStateTab.js
@@ -15,7 +15,7 @@ const StatisticStateTab = ({ testingData, indiaData, stateDataLatest }) => {
 	);
 	return (
 		<Tabs tabBarExtraContent={
-			(<div>7th July, 8:15 PM</div>)
+			(<div>7th July, 8:48 PM</div>)
 		}
 		>
 			<TabPane
