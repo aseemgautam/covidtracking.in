dc58e22a91d5e44b4a6357574c6cb97394bd0968
Aseem Gautam <aseem.gautam@gmail.com>
2020-07-26 20:44:15 +0530

    live update

--- components/HomePageTabsFirst.js
+++ components/HomePageTabsFirst.js
@@ -15,7 +15,7 @@ const HomePageTabs = ({ testingData, indiaData, stateDataLatest }) => {
 	);
 	return (
 		<Tabs tabBarExtraContent={
-			(<div>26th July, 7:48 PM</div>)
+			(<div>26th July, 8:45 PM</div>)
 		}
 		>
 			<TabPane
