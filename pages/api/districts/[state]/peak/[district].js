ab63fd71a74fd25bdab50fa14d810821207ef5bb
Aseem Gautam <aseem.gautam@gmail.com>
2020-08-07 17:47:54 +0530

    updated peak API

--- pages/api/districts/[state]/peak/[district].js
+++ pages/api/districts/[state]/peak/[district].js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import papa from 'papaparse';
 import Districts from '../../../../../classes/Districts';
 
@@ -8,11 +9,16 @@ export default async (req, res) => {
 	const peakData = (await Districts.single(state, district)).map(record => {
 		return {
 			Date: record.date,
+			Zero: 0,
+			'New Cases': record.newCases,
 			'New Cases 7 Day MA': record.newCases7DayMA,
 			'Recoveries 7 Day MA': record.newRecovered7DayMA,
 			Peak: record.newCases7DayMA - record.newRecovered7DayMA
 		};
 	});
 	// res.end(`State: ${state} District: ${district}`);
+	if (_.last(peakData).Peak === _.nth(peakData, -2).Peak) {
+		peakData.pop();
+	}
 	res.end(papa.unparse(peakData));
 };
