effb6148b12fd729eed52f2907f34938818f7859
Aseem Gautam <aseem.gautam@gmail.com>
2020-08-09 11:19:09 +0530

    morning update

--- components/HomePageTabsFirst.js
+++ components/HomePageTabsFirst.js
@@ -22,7 +22,7 @@ const HomePageTabs = ({ testingData, indiaData, stateDataLatest, buildTime }) =>
 		>
 			<TabPane
 				tab={
-					(<div className="card-tab-title">Statistics ON (07/08)</div>)
+					(<div className="card-tab-title">Statistics ON (08/08)</div>)
 				}
 				key="1"
 			>
