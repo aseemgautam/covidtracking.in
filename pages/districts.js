86fe69f0ee18e4cf36b94ab8fb8eb21d80811a9f
Aseem Gautam <aseem.gautam@gmail.com>
2021-04-12 15:39:52 +0530

    districts chart index from google spreadsheet

--- pages/districts.js
+++ pages/districts.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line import/no-unresolved
 import DWChart from 'react-datawrapper-chart';
+import fetch from 'isomorphic-unfetch';
 
-const Districts = () => {
+const Districts = ({ chartIndex }) => {
 	return (
 		<>
 			<h2>COVID-19 District Tracker India</h2>
@@ -13,9 +14,20 @@ const Districts = () => {
 				you can see every districts progress towards containing COVID-19.
 			</div>
 			<br />
-			<DWChart title="Chart" src="//datawrapper.dwcdn.net/sWfDe/16/" />
+			<DWChart title="Chart" src={`//datawrapper.dwcdn.net/sWfDe/${chartIndex}/`} />
 		</>
 	);
 };
+
+export async function getStaticProps() {
+	// eslint-disable-next-line max-len
+	const res = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQRyjPj_VXGIOYnCy5eoy3YcN9yA_yFKWd4AdkMXFam62N4Ik-D6A6cwFXt2N2LwpncJEd-dFn7s5Ez/pub?gid=1843614518&single=true&output=csv');
+	const csv = await res.text();
+	return {
+		props: {
+			chartIndex: csv
+		}
+	};
+}
 
 export default Districts;
